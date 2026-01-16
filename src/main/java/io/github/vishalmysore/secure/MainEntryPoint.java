2e50f5fbed2032f213f3d75f918ea1f805c78f4b
Vishal Row Mysore <visrow@gmail.com>
2026-01-16 17:40:28 -0500

    changes for a2ui

--- src/main/java/io/github/vishalmysore/secure/MainEntryPoint.java
+++ src/main/java/io/github/vishalmysore/secure/MainEntryPoint.java
@@ -4,12 +4,13 @@ package io.github.vishalmysore.secure;
 import io.github.vishalmysore.SecureSpringJSONController;
 import io.github.vishalmysore.a2a.domain.JsonRpcRequest;
 import io.github.vishalmysore.common.server.SpringAwareJSONRpcController;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
 /** Expose the Json-RPC endpoint for the tasks
  *  This will handle all the JSON RPC Requests for a2a such as
  *  tasks/send
@@ -35,9 +36,9 @@ public class MainEntryPoint extends SecureSpringJSONController {
     }
 
     @PostMapping
-    public Object handleRpc(@RequestBody JsonRpcRequest request) {
+    public Object handleRpc(@RequestBody JsonRpcRequest request, HttpServletRequest httpServletRequest) {
         log.info(request.toString());
-        Object result = super.handleRpc(request);
+        Object result = super.handleRpc(request, httpServletRequest);
         log.info("Result: " + result);
         return result;
     }
